0b6d1e8b4627a476adcb2e83489b3567b7b97998
Kelly Campen <kelly.campen@gmail.com>
2022-12-02 09:21:01 +1300

    day 1 commit

--- day1/day1pt1.py
+++ day1/day1pt1.py
@@ -3,14 +3,14 @@ doublenewlinechar = "\n\n"
 
 with open("day1test.txt", "r") as f:
     data = f.read()
-    lines = data.split(doublenewlinechar)
+    allElves = data.split(doublenewlinechar)
 
-for elfIndex, elf in enumerate(lines):
+for elfIndex, elf in enumerate(allElves):
     if newlinechar in elf:
         snacks = elf.split(newlinechar)
         result = sum(map(int, snacks))
-        lines[elfIndex] = result
+        allElves[elfIndex] = result
 
-mylist = list(map(int, lines))
+mylist = list(map(int, allElves))
 finalResult = max(mylist)
 print(finalResult)
--- day1/day1pt2.py
+++ day1/day1pt2.py
@@ -4,14 +4,15 @@ doublenewlinechar = "\n\n"
 
 with open("day1input.txt", "r") as f:
     data = f.read()
-    lines = data.split(doublenewlinechar)
+    allElves = data.split(doublenewlinechar)
 
-for elfIndex, elf in enumerate(lines):
+for elfIndex, elf in enumerate(allElves):
     if newlinechar in elf:
         snacks = elf.split(newlinechar)
         result = sum(map(int, snacks))
-        lines[elfIndex] = result
+        allElves[elfIndex] = result
 
+mylist = list(map(int, allElves))
 mylist.sort(reverse=True)
 finalResultpt2 = mylist[0] + mylist[1] + mylist[2]
 print(finalResultpt2)
